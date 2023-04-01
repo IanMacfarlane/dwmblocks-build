adfadbe054f9543068a171ce1b95efaf36781bc0
ianmacfarlane <iandmacfarlane13@gmail.com>
2023-04-01 07:16:22 -0600

    vpnstatus and hardware monitor scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date '+ %I:%M %p | %a %b %d '",					5,		0},
+	{"", "fanspeed", 5, 0},
+	{"", "cputemp", 5, 0},
+	{"", "vpnstatus",	10, 0},
+	{"", "date '+ %I:%M %p | %a %b %d '",					5,		0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
